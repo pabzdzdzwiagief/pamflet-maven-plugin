eb76d1552104aaef1012acbaeae314e4a65421ec
pabzdzdzwiagief <pabzdzdzwiagief@gmail.com>
2016-12-19 21:45:38 +0100

    Implement `preview` goal

---
+++ src/main/java/com/github/pabadzdzwiagief/pamflet/PamfletPreviewMojo.java
@@ -0,0 +1,20 @@
+package com.github.pabadzdzwiagief.pamflet;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import pamflet.Pamflet;
+
+@Mojo(name = "preview")
+public final class PamfletPreviewMojo extends AbstractMojo {
+    @Parameter(defaultValue = "src/site")
+    private File sourceDirectory;
+
+    public void execute() throws MojoExecutionException {
+        Pamflet.main(new String[]{sourceDirectory.toString()});
+    }
+}
